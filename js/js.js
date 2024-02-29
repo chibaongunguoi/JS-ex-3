454efeba9cf0fdae7b816c43ec139f2d7ed9dadc
chibaongunguoi <chibaongunguoi@gmail.com>
2024-02-29 08:25:16 +0700

    full

--- js/js.js
+++ js/js.js
@@ -3,56 +3,71 @@ const value=document.querySelector("input")
 const ul=document.querySelector("ul")
 const show=document.getElementById("result")
 var target;
+ //localStorage.clear()
 var count=0;
 var countelement=0;
-let object=JSON.parse(localStorage.getItem("user"))
+var object=JSON.parse(localStorage.getItem("user"))
+var objectmid={}
 if (object==null)
 object={}
 else{
     for (i in object){
         const li=document.createElement("li");
         countelement+=1
-        li.innerHTML=`<a data-n=${countelement} href="javascript:void(0)">${object[i]}
+        li.innerHTML=`<a data-type="${object[i][1]}" data-n=${countelement} href="javascript:void(0)">${object[i][0]}
           <span><i class="fa-thin fa-trash-can" ></i></span>
           <span><i class="fa-solid fa-pencil"></i></span>
         </a> 
         <hr />`
+          if (object[i][1]=="off")  {li.children[0].style.textDecoration="line-through" ;count+=1}
+        objectmid[countelement]=object[i]
        ul.appendChild(li)
        addevent();
+       updateshow();
     }
+    object=objectmid
+    update()
 }
-
+function checkcount(a){
+    if (a=="off") count-=1
+    updateshow();
+}
 function update(){
     localStorage.setItem("user",JSON.stringify(object))
-    let data=JSON.parse(localStorage.getItem("user"))
 }
 function write(){
     const li=document.createElement("li");
     countelement+=1
-    li.innerHTML=`<a data-n=${countelement} href="javascript:void(0)">${value.value}
+    li.innerHTML=`<a data-type="on" data-n=${countelement} href="javascript:void(0)">${value.value}
       <span><i class="fa-thin fa-trash-can" ></i></span>
       <span><i class="fa-solid fa-pencil"></i></span>
     </a> 
     <hr />`
-    object[countelement]=value.value
+    object[countelement]=[value.value,"on"]
     update();
    ul.appendChild(li)
    value.value="";
 }
+
 
 function edit(){
     target.innerHTML=`${value.value}<span><i class="fa-thin fa-trash-can" ></i></span>
     <span><i class="fa-solid fa-pencil"></i></span>`
     value.nextElementSibling.value="ADD TASK";
     form.dataset.type="add";
+    value.value=""
     target.children[0].addEventListener("click",function(){
         this.parentElement.removeEventListener("click",change);
+        const g=this.parentElement.dataset.n
+        delete(object[g])
+        update();
+        checkcount(this.parentElement.dataset.type)
         this.parentElement.parentElement.remove()
         })
         target.children[1].addEventListener("click",function(){
             this.parentElement.removeEventListener("click",change);
-            value.value=this.parentElement.innerText;
-            value.nextElementSibling.value="EDIT TASK";
+            value.value=this.parentElement.innerText
+            value.nextElementSibling.value="EDIT TASK"
             form.dataset.type="edit";
             target=this.parentElement
             })
@@ -64,16 +79,25 @@ form.addEventListener("submit",function (e){
     write();
    addevent();
 })
-
-function change(){
-    if ("line-through"==this.style.textDecoration){
-    this.style.textDecoration="none";count-=1;
-    }
-else
-{this.style.textDecoration="line-through";count+=1;}
+function updateshow(){
     if (count>0)
     show.innerHTML=`Yeah, ${count} task completed!`
 else show.innerHTML=``;
+}
+function change(){
+    if (this.dataset.type=="off"){
+    this.style.textDecoration="none";
+    count-=1;
+    this.dataset.type="on"
+    }
+else
+{
+    this.style.textDecoration="line-through";
+    count+=1;
+    this.dataset.type="off"}
+    updateshow();
+    object[this.dataset.n][1]=this.dataset.type
+    update();
 }
 
 function check(){
@@ -88,6 +112,8 @@ function addevent(){
         a.removeEventListener("click",change);
         const g=this.parentElement.dataset.n
         delete(object[g])
+        update();
+        checkcount(this.parentElement.dataset.type);
     this.parentElement.parentElement.remove()
     })
     c.addEventListener("click",function(){
